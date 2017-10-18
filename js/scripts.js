af59179b281bdbb8446a4debd9e065c00f8354e4
David Mrabu <davidjblaze@hotmail.com>
2017-10-18 10:09:54 +0300

    Form is complete

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
         var verbInput = $("input#verb").val();
         var nounInput = $("input#noun").val();
 
-         $(".person1").append("blah blah");
-         $(".person2").append("blah blah");
-         $(".animal").append("blah blah");
-         $(".exclamation").append("blah blah");
-         $(".verb").append("blah blah");
-         $(".noun").append("blah blah");
+       $(".person1").text(person1Input);
+       $(".person2").text(person2Input);
+       $(".animal").text(animalInput);
+       $(".exclamation").text(exclamationInput);
+       $(".verb").text(verbInput);
+       $(".noun").text(nounInput);
 
          $("#story").show();
 
